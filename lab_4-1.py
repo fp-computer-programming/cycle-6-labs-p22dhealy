55d1cf3e145eb9e5e7677a2b3632c10172cd3391
p22dhealy <p22dhealy@fairfieldprep.org>
2021-11-12 11:03:50 -0500

    submit lab 4

---
+++ lab_4-1.py
@@ -0,0 +1,13 @@
+#author: DMH 11/12
+
+colors = ['red','blue','yellow','green']
+
+colors.extend(['purple','brown','teal'])
+
+list.count('yellow')
+
+colors.insert(3,'orange')
+
+colors.clear()
+
+colors.count('red')
---
+++ lab_4-2.py
@@ -0,0 +1,18 @@
+#author: DMH 11/12
+
+subjects = ['Math','English','Spanish']
+
+subjects.append('History')
+
+subjects.index('Spanish')
+
+subjects.sort()
+
+subjects2 = subjects.copy()
+
+print(subjects)
+
+subjects2.reverse()
+
+print(subjects2)
+
